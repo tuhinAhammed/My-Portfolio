1f0e88dd30ab54566f1463695792cd91c85a9bb9
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-06-06 12:23:28 +0600

    FirstCommit -- ProjectSetup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+// bootstrap linked here
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home';
+
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
