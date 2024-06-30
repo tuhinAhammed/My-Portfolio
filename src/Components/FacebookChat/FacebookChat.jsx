0fba65c18656bb8cfdc9b65629d7cbc16c0993b2
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-07-01 00:52:41 +0600

    facebook messanger update

--- src/Components/FacebookChat/FacebookChat.jsx
+++ src/Components/FacebookChat/FacebookChat.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 const FacebookChat = () => {
   return (
     <div>
-        <MessengerCustomerChat
-    pageId="153542789835248"
-    appId="<460086386839803>"
-  />
+      <MessengerCustomerChat
+        pageId="153542789835248"
+        appId="460086386839803"
+      />
     </div>
-  )
+  );
 }
 
-export default FacebookChat
+export default FacebookChat;
