c5e5d208dbec39cb4039851d105764f7946c21bb
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-07-01 11:46:10 +0600

    react-facebook-messenger-plugin

--- src/Components/FacebookChat/FacebookChat.jsx
+++ src/Components/FacebookChat/FacebookChat.jsx
@@ -1,15 +1,16 @@
-'use client'
 import React from 'react';
-import MessengerChatPlugin from 'react-messenger-chat-plugin';
-
+import { MessengerChat } from 'react-facebook-messenger-plugin';
 const FacebookChat = () => {
     console.log("okkkkk");
   return (
-    <div>
+    <div className="z-[99999] py-6 ">
       <h1>Welcome to My Website</h1>
-      <MessengerChatPlugin
+      <MessengerChat
         pageId="153542789835248"
-        appId="460086386839803"
+        appId="460086386839803D"
+        themeColor="#6699cc"
+        loggedInGreeting="Hi! How can we help you?"
+        loggedOutGreeting="Hi! Please log in to chat with us."
       />
     </div>
   );
