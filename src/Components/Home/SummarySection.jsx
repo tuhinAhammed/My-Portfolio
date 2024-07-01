e4e002b88049545050eb9f90836a6c0f19067149
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-07-01 19:22:17 +0600

    resume add

--- src/Components/Home/SummarySection.jsx
+++ src/Components/Home/SummarySection.jsx
@@ -7,26 +7,26 @@ import { Bounce, Flip, Slide, ToastContainer, Zoom, toast } from 'react-toastify
 import 'react-toastify/dist/ReactToastify.css';
 import resume from "../../assets/Resume/tuhinResume.pdf"
 const SummarySection = () => {
-  const handleDownload = () => {
-    // toast.success("Download successfully done!", {
-    //   position: "bottom-center",
-    //   autoClose: 10000,
-    //   hideProgressBar: false,
-    //   closeOnClick: true,
-    //   pauseOnHover: true,
-    //   draggable: true,
-    //   progress: undefined,
-    //   theme: "dark",
-    //   transition: Bounce,
-    // });
+  // const handleDownload = () => {
+  //   // toast.success("Download successfully done!", {
+  //   //   position: "bottom-center",
+  //   //   autoClose: 10000,
+  //   //   hideProgressBar: false,
+  //   //   closeOnClick: true,
+  //   //   pauseOnHover: true,
+  //   //   draggable: true,
+  //   //   progress: undefined,
+  //   //   theme: "dark",
+  //   //   transition: Bounce,
+  //   // });
 
-  }
+  // }
   return (
     <div className='color-primary '>
       <MidTitle text="Title" className="group-hover:text-theme text-theme " />
       <MidTitle text="MERN Stack" className=" text-3xl group-hover:text-theme duration-300" />
       <MidTitle text="Developer" className="text-right text-3xl opacity-[0.5] z-auto pb-7" />
-      <a href={resume} download onClick={handleDownload}>
+      <a href={resume} download >
         <div className="resume cursor-pointer flex justify-between items-center  group transition  delay-1000">
           <MinTitle text="My Resume" className="group-hover:text-theme duration-300" />
           <FaDownload className='text-2xl animate-bounce text-primary group-hover:text-theme' />
