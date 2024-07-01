f9adc620313d160dab84b4a97881b27a112db2c0
tuhinAhammed <tuhinahamed2030@gmail.com>
2024-07-01 10:30:08 +0600

    update react-facebook

--- src/Components/FacebookChat/FacebookChat.jsx
+++ src/Components/FacebookChat/FacebookChat.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const FacebookChat = () => {
     console.log("okkkkk");
   return (
     <div>
-      okkkk
+      <FacebookProvider appId="460086386839803" chatSupport>
+        <CustomChat pageId="153542789835248" minimized={true}/>
+      </FacebookProvider>  
     </div>
   );
 }
